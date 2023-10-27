ed9d7866b68411b89e2108c046185702ff84a9ef
loujessler <danilin055@yahoo.com>
2023-10-27 16:45:33 +0400

    Add back-end

---
+++ backend/config.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG") == "True"
+
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT") if DEBUG else ''
+
+# CORS_WHITELIST = os.environ.get("CORS_WHITELIST").split(",") if os.environ.get("CORS_WHITELIST") else []
+
